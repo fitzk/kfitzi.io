75e854dd659d95a4b673cd204cfd97de0487c8b7
fitzk <kayla.fitzsimmons@protonmail.com>
2017-02-04 16:47:47 -0800

    update about

--- client/about/index.js
+++ client/about/index.js
@@ -1,36 +1,33 @@
 import React from "react"
-import styled from "styled-components";
-import { Row, Column, H1 } from "components"
+import styled from "styled-components"
+import { Row, Column, H1, RowColumn } from "components"
 
 /*esfmt-ignore-start*/
-const Paragraph = styled(Row)`
-	font-family: Roboto, Sans-serif;
-	margin: 10px;
-	max-width: 50vw;
-	margin-bottom: 20vh;
+const Container = styled(Column)`
+	padding: 2em;
+	height: 90vh;
 `
-const FontContainer = styled.div`
-	height: 1em
-	margin: 5px;
-`
-/*esfmt-ignore-end*/
-export const About = props => {
-	/*esfmt-ignore-start*/
-	const header = <H1> about </H1>;
-	/*esfmt-ignore-end*/
 
-	return <Column>
-          <Column alignStart>
-            <Row>
-              { header }
-            </Row>
-            <Paragraph>
-              { `California native && SF Bay Area software developer. ` }
-              <FontContainer>
-                <a href="https://twitter.com/kfitzikfitzi"><i className="fa fa-twitter"
-                                                              aria-hidden="true"></i></a>
-              </FontContainer>
-            </Paragraph>
-          </Column>
-        </Column>
+const Paragraph = styled.p`
+	font-family: Roboto, sans-serif;
+	font-size: 1.25rem;
+	color: white;
+	font-weight: 200;
+	overflow: hidden;
+	max-width: 700px;
+	height: auto;
+`/*esfmt-ignore-end*/
+
+export const About = props => {
+	return <Container alignStart
+                   justifyCenter>
+          <H1 children="about" />
+          <Paragraph>
+            { `California native and SF bay area software developer.
+								I use React, Redux, Webpack, Babel, and Styled Components to build modern
+								web applications. I spend my free time working on side projects, as well as reading and writing
+								about software development.
+								` }
+          </Paragraph>
+        </Container>
 }
